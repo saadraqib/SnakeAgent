1b5b63c5f06cee5e489da6a12005844ea7dd94e3
saadraqib <saad.rqaziz@gmail.com>
2025-02-13 16:57:53 +0300

    added config file and assigned constant variables inside it to use it easily

---
+++ config.py
@@ -0,0 +1,12 @@
+direction = {"up":(-1, 0), "down":(1,0), "left":(0, -1), "right":(0,1)}
+wrong_directions ={"up":"down", "down":"up", "left":"right", "right":"left"}
+
+rows = 15
+cols = 15
+
+snake = [[(rows//2), (cols//2)-3]]
+
+target = [(rows//2),(cols//2)+3]
+
+direct_key = "right"
+
